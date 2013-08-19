4fb998ff9e48c2cdc53e9123b7891ca223c59f8a
Cameron Bytheway <cameron@theflokk.com>
2013-08-19 14:20:42 -0600

    Fix ie for lack of toISOString

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 /**
  * Module dependencies
  */
-var defaults = require("defaults");
+
+var defaults = require('defaults');
+var pad = require('pad');
 
 // Default config
 var config = {
   facility: 14,
   severity: 6,
   version: 1,
-  app_name: "app",
-  proc_id: "web.1"
+  hostname: 'nohost',
+  app_name: 'browser',
+  proc_id: 'web'
+};
+
+function toISOString(date) {
+  return date.getUTCFullYear()
+    + '-' + pad(date.getUTCMonth() + 1 + '', 2, '0')
+    + '-' + pad(date.getUTCDate() + 1 + '', 2, '0')
+    + 'T' + pad(date.getUTCHours() + 1 + '', 2, '0')
+    + ':' + pad(date.getUTCMinutes() + 1 + '', 2, '0')
+    + ':' + pad(date.getUTCSeconds() + 1 + '', 2, '0')
+    + '.' + String((date.getUTCMilliseconds()/1000).toFixed(3)).slice(2, 5)
+    + 'Z';
 };
 
 module.exports = exports = function(opts) {
@@ -17,12 +31,12 @@ module.exports = exports = function(opts) {
 
   defaults(opts, config);
 
-  var prefix = "<"+(opts.priority || opts.facility*8+opts.severity)+">"+opts.version
-    , system = [opts.hostname, opts.app_name, opts.proc_id, "- -"].join(" ");
+  var prefix = ['<', (opts.priority || opts.facility * 8 + opts.severity), '>', opts.version].join('')
+    , system = [opts.hostname, opts.app_name, opts.proc_id, '- -'].join(' ');
 
   return function() {
-    var message = Array.prototype.join.call(arguments, " ");
-    var msg = [prefix, (new Date).toISOString(), system, message].join(" ");
-    return [msg.length,msg].join(" ");
+    var message = Array.prototype.join.call(arguments, ' ');
+    var msg = [prefix, toISOString(new Date), system, message].join(' ');
+    return [msg.length, msg].join(' ');
   };
 };
